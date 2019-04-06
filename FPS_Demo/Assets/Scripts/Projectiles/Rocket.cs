1d462a62de02826bb252a88ccaea85ec359c7d68
Chris Wenzlick <cwenzlick01@gmail.com>
2019-04-06 18:27:36 -0500

    Development Sandbox
    Added a sandbox scene with floor, walls, a platform, and a ramp. Created a player with a simple gun prefab that launches small rockets.
    
    Note: The rockets currently explode when fired below a certain angle (most likely colliding with player), and the explosion particle systems persist after finishing their animations.

---
+++ FPS_Demo/Assets/Scripts/Projectiles/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed = 2000.0f;
+
+    public GameObject explosionPrefab;
+
+    void OnCollisionEnter(Collision c)
+    {
+        // Instantiate the explosion
+        // Note: the explosion is not destroyed after finishing, and the rocket appears
+        // to collide with the player when fired below a certain angle; fix both prior
+        // to using in a full game
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FPS_Demo/Assets/Scripts/Weapons/Rocket Launcher/Shoot_Rocket.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_Rocket : MonoBehaviour
+{
+    public GameObject rocketPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newRocket = (GameObject)Instantiate(rocketPrefab, transform.position, transform.parent.rotation);
+            Physics.IgnoreCollision(GetComponent<Collider>(), newRocket.GetComponent<Collider>());
+            float force = newRocket.GetComponent<Rocket>().speed;
+            newRocket.GetComponent<Rigidbody>().AddForce(newRocket.transform.forward * force);
+        }
+    }
+}
